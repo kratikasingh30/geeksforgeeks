2ab85aaa56d21477bb6c003c8e797e88573000c1
Kratika Singh <114504847+kratikasingh30@users.noreply.github.com>
2025-06-13 11:48:27 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Second Largest/second-largest.java
+++ Difficulty: Easy/Second Largest/second-largest.java
@@ -1,52 +1,25 @@
-//{ Driver Code Starts
-// Initial Template for Java
-import java.io.*;
-import java.util.*;
-import java.util.stream.Collectors;
-
-public class Main {
-
-    public static void main(String[] args) throws Exception {
-        Scanner sc = new Scanner(System.in);
-        int t = Integer.parseInt(sc.nextLine());
-        while (t-- > 0) {
-            String[] arr1Str = sc.nextLine().split(" ");
-            int[] arr = Arrays.stream(arr1Str).mapToInt(Integer::parseInt).toArray();
-            Solution ob = new Solution();
-            int ans = ob.getSecondLargest(arr);
-            System.out.println(ans);
-
-            System.out.println("~");
-        }
-    }
-}
-
-// } Driver Code Ends
-
-
-
-
-// User function Template for Java
-
 class Solution {
     public int getSecondLargest(int[] arr) {
-        // Code Here
+        // code here
         int max = arr[0];
-        for(int i =0;i<arr.length;i++){
+        int smax =-1;
+        for (int i =0;i<arr.length;i++){
             if(arr[i]>max){
                 max=arr[i];
             }
-        }
-        int second_largest=-1;
-        for(int i=0;i<arr.length;i++){
-            if(arr[i]<max&&arr[i]>second_largest){
-                second_largest=arr[i];
-            }
-        }
         
-       if(max==second_largest) return -1;
-       else{
-           return second_largest;
-       }
-    }
+        } 
+        for(int i = 0 ;i<arr.length;i++){
+            if(arr[i]<max &&arr[i]>smax){
+                smax=arr[i];
+            }
+            
+        }
+        if(max==smax){
+            return -1;
+        }
+        else{
+            return smax;
+        }
+}
 }
